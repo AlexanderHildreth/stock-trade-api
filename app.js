2bbcdb771f6bbb978508c20ef53958ff8fab35e3
Alexander Hildreth <zander.hildreth@gmail.com>
2020-04-29 17:15:41 +0200

    basic rate lmiting, hpp and cors

--- app.js
+++ app.js
@@ -2,9 +2,12 @@
 const dotenv            = require('dotenv')
 const cookieParser      = require('cookie-parser')
 const colours           = require('colors')
+const cors              = require('cors')
 const express           = require('express')
+const expressRateLimit  = require('express-rate-limit')
 const fileUpload        = require('express-fileupload')
 const helmet            = require('helmet')
+const hpp               = require('hpp')
 const fs                = require('fs')
 const morgan            = require('morgan')
 const mongoSanitize     = require('express-mongo-sanitize')
@@ -22,17 +25,24 @@ const reviews           = require('./routes/reviews')
 const users             = require('./routes/users')
 // const vars
 const app               = express()
-const PORT              = process.env.PORT || 5000
+const limiter           = expressRateLimit({
+    windowMs: 10 * 60 * 1000, // !0 minutes
+    max: 100
+})
+const port              = process.env.PORT || 5000
 const accessLogStream   = fs.createWriteStream(path.join(__dirname, '/var/logs/access.log'), { flags: 'a' })
 
 // App middlewares
-app.use(express.json())
-app.use(cookieParser())
-connectDB()
-app.use(mongoSanitize())
-app.use(helmet())
-app.use(xssClean())
 process.env.NODE_ENV === 'development' ? app.use(morganLogging) : app.use(morgan('combined', { stream: accessLogStream }))
+connectDB()
+app.use(cookieParser())
+app.use(cors())
+app.use(helmet())
+app.use(hpp())
+app.use(limiter)
+app.use(mongoSanitize())
+app.use(xssClean())
+app.use(express.json())
 app.use(fileUpload())
 app.use(express.static(path.join(__dirname, 'public')))
 
@@ -46,8 +56,8 @@ app.use('/api/v1/users', users)
 // Routes middlewares
 app.use(errorHandler)
 
-const server = app.listen(PORT, () => {
-    console.log(`[app] Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.underline.bold)
+const server = app.listen(port, () => {
+    console.log(`[app] Server running in ${process.env.NODE_ENV} mode on port ${port}`.yellow.underline.bold)
 })
 
 // Handle unhandles promis rejections
