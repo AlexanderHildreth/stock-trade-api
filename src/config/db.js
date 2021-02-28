8590b669c3eb3ad6405e8f11297018ee84bb0401
AlexanderHildreth <zander.hildreth@gmail.com>
2021-02-28 16:54:19 +0100

    Updated db connection logic

--- src/config/db.js
+++ src/config/db.js
@@ -3,12 +3,13 @@ const sqlite3 = require('sqlite3')
 const sqlite  = require('sqlite')
 
 async function createDbConn (app) {
-  const DbConn = await sqlite.open({
+  app.set('db', await sqlite.open({
     filename: ':memory:',
     driver: sqlite3.Database
-  })
+  }))
   
-  app.set('db', DbConn)
+  const DbConn = app.get('db')
+
   DbConn.run(
     `CREATE TABLE IF NOT EXISTS trades (
       id int primary key,
@@ -27,8 +28,6 @@ async function createDbConn (app) {
     DbConn.run(`CREATE INDEX IF NOT EXISTS time_index ON trades (timestamp)`);
     DbConn.run(`CREATE INDEX IF NOT EXISTS user_index ON trades (user_id)`);
     DbConn.run(`CREATE INDEX IF NOT EXISTS type_index ON trades (type)`);
-
-    return DbConn;
   })
 }
 
