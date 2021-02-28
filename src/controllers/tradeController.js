8001d164d56c687a6a5d74785a075897d89646a3
AlexanderHildreth <zander.hildreth@gmail.com>
2021-02-28 18:04:17 +0100

    refactored trade controller to fix issues

--- src/controllers/tradeController.js
+++ src/controllers/tradeController.js
@@ -17,7 +17,7 @@ module.exports = {
                     return reject(err);
                 };
                 
-                resolve(res.map(databaseUtil.format(res)));
+                resolve(res.map(databaseUtil.format));
             });
             
         })
@@ -29,79 +29,79 @@ module.exports = {
     createTrade: req => {
         return new Promise((resolve, reject) => {
             const DB = databaseUtil.getDbConn(req);
-            
-            const statement = DB.prepare(
-                'INSERT INTO trades (id, type, user_id, user_name, symbol, shares, price, timestamp) VALUES (?, ?, ?, ?, ?, ?, ?, ?)',
-                [
-                    req.body.id,
-                    req.body.type,
-                    req.body.user.id,
-                    req.body.user.name,
-                    req.body.symbol,
-                    req.body.shares,
-                    req.body.price,
-                    req.body.timestamp
-                ]
-            );
+            length = req.body.length
+
+            req.body.forEach(element => {
+                const statement = DB.prepare(
+                    'INSERT INTO trades (id, type, user_id, user_name, symbol, shares, price, timestamp) VALUES (?, ?, ?, ?, ?, ?, ?, ?)'         
+                ).bind([
+                    element.id,
+                    element.type,
+                    element.user.id,
+                    element.user.name,
+                    element.symbol,
+                    element.shares,
+                    element.price,
+                    element.timestamp
+                ]);
                 
-            result = DB.all('SELECT id FROM trades WHERE id = ? LIMIT 1', (err, res) => {
-                if (err) {
-                    return reject(err);
-                };
-
-                if (res.length > 0) {
-                    return reject()
-                }
-
-                statement.run(err => {
-                    if (err) reject(err);
+                result = DB.all('SELECT id FROM trades WHERE id = ? LIMIT 1', (err, res) => {
+                    if (err) {
+                        return reject(new ErrorResponse(`There was a error - ${err.message}`, 400));
+                    };
+                    
+                    if (res.length > 0) {
+                        return reject()
+                    }
+                    
+                    statement.run(err => {
+                        if (err) reject(new ErrorResponse(`There was a error - ${err.message}`, 400));
+                        
+                    });
                 });
             });
-            
-            res.status(201)
-            .json({
-                success: true,
-                data: result
-            })
 
             resolve()
         })
-    }    
-}
-/*
-// @desc    Delete all trades
-// @route   DELETE /api/v1/trades/:id
-// @access  Public
-exports.deleteAllTrades = asyncHandler(async (req, res, next) => {
-    const DB = databaseUtil.getDbConn(req);
+    },   
+    // @desc    Delete all trades
+    // @route   DELETE /api/v1/trades/:id
+    // @access  Public
+    deleteAllTrades: req => {
+        return new Promise((resolve, reject) => {
+            const DB = databaseUtil.getDbConn(req);
 
-    DB.run('DELETE FROM trades; VACUUM;', err => {
-        if (err) {
-            return next(new ErrorResponse(`There was a error - ${err.message}`, 400));
-        };
-    });
+            DB.run('DELETE FROM trades; VACUUM;', err => {
+                if (err) {
+                    return reject(new ErrorResponse(`There was a error - ${err.message}`, 400));
+                };
 
-    res.status(200)
-        .json({
-            success: true,
-            data: {}
+                resolve()
+            });
         })
-})
+    },
+    // @desc    Get all trades by user ID
+    // @route   GET /api/v1/trades//user/userIid
+    // @access  Public
+    getTradesByUserId: (req, userId) => {
+        return new Promise((resolve, reject) => {
+            const DB = databaseUtil.getDbConn(req);
 
-// @desc    Get all trades by user ID
-// @route   GET /api/v1/trades//user/userIid
-// @access  Public
-exports.getTradesByUserId = asyncHandler(async (req, res, next) => {
-    const DB = databaseUtil.getDbConn(req);
+            const statement = DB.prepare(
+            `SELECT * FROM trades WHERE user_id = ? ORDER BY id`
+            );
 
-    const statement = DB.prepare(
-    `SELECT * FROM trades WHERE user_id = ? ORDER BY id`
-    );
-    statement.all(userID, (err, res) => {
-    if (err || res.length === 0) {
-        return next(new ErrorResponse(`There was a error - ${err.message}`, 400));
-    };
+            statement.all(userID, (err, res) => {
+                if (err) {
+                    return reject(new ErrorResponse('There was a error - ${err}', 400));
+                }
 
-    res.map(databaseUtil.format);
-    });
-});*/
+                if (res.length === 0) {
+                    return reject(new ErrorResponse('There was a error', 400));
+                }
+                
+                res.map(databaseUtil.format);
+            });
+        });
+    }
+}
